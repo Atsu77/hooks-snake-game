55457a3db2943cebb7a4f8fc51f851c9cd3c36d9
Atsu <kamnknon@gmail.com>
2021-06-26 16:33:52 +0900

    snakeが上に動く処理を追加

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,10 @@
-export const initFields = (fieldsSize) => {
+export const initFields = (fieldsSize, initialPosition) => {
   const fields = [];
   for (let i = 0; i < fieldsSize; i++) {
     const cols = new Array(fieldsSize).fill("");
     fields.push(cols);
   }
   
-  fields[17][17] = "snake";
+  fields[initialPosition.y][initialPosition.x] = 'snake'
   return fields;
 };
